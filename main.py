e697cb4bab3fa01442f053a823e6e9888c23585a
noaglte <noahloute1995@gmail.com>
2025-05-21 15:47:30 +0200

    Detection auto des COGS

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print("Bot allumé !")
 
-    await bot.load_extension("cogs.info")
-    await bot.load_extension("cogs.admin")
-
     try:
         synced = await bot.tree.sync()
         print(f"Commandes slash synchro : {len(synced)}")
     except Exception as e:
         print(e)
+
+@bot.event
+async def setup_hook():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            cog_name = f"cogs.{filename[:-3]}"
+            try:
+                await bot.load_extension(cog_name)
+                print(f"Cog chargé : {cog_name}")
+            except Exception as e:
+                print(f"❌ Erreur lors du chargement de {cog_name} : {e}")
 
 bot.run(os.getenv('DISCORD_TOKEN'))
