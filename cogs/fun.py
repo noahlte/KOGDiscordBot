4c5469ea62d305fde5112b25a1cef7f985f34728
noaglte <noahloute1995@gmail.com>
2025-05-21 16:31:52 +0200

    Amélioration de la commande roll

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,9 +8,18 @@ class Fun(commands.Cog):
         self.bot = bot
     
     @app_commands.command(name="roll", description=("Choisis un chiffre aléatoire entre 1 et 100"))
-    async def roll(self, interaction: discord.Interaction):
-        number = random.randint(0, 100)
-        await interaction.response.send_message(f"🎲 Tu as tiré le chiffre : {number}")
+    @app_commands.describe(
+        min = "Valeur minimal",
+        max = "Valeur maximal"
+    )
+    async def roll(self, interaction: discord.Interaction, min: int = 1, max: int = 100):
+
+        if min >= max:
+            await interaction.response.send_message("❌ La valeur minimal ne peut être supérieur ou égal à la valeur maximal", ephemeral=True)
+            return
+
+        number = random.randint(min, max)
+        await interaction.response.send_message(f"🎲 Tu as tiré un nombre entre {min} et {max}, le résultat est : {number}")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Fun(bot))
