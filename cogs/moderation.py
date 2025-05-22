b1f9a005ac9a2dd5d0b8a74e54df4253b601411c
noahlte <noahloute1995@gmail.com>
2025-05-22 20:58:53 +0200

    MAJ Mineur

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,13 +3,6 @@ from discord import app_commands
 from discord.ext import commands
 
 from datetime import datetime
-import asyncio
-import re
-import time
-
-import os
-import json
-
 
 class Moderation(commands.Cog):
 
